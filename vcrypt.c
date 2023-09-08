bbedf0e05afddc046eb44764b8732cd8a8efece4
vivrehalclear <dhimanvivek777@gmail.com>
2023-09-09 00:55:11 +0530

    adding vcrypt.c

---
+++ vcrypt.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+#define ROTR(x, n) (((x) >> (n)) | ((x) << (32 - (n))))
+#define Ch(x, y, z) (((x) & (y)) ^ (~(x) & (z)))
+#define Maj(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
+#define Sigma0(x) (ROTR(x, 2) ^ ROTR(x, 13) ^ ROTR(x, 22))
+#define Sigma1(x) (ROTR(x, 6) ^ ROTR(x, 11) ^ ROTR(x, 25))
+#define sigma0(x) (ROTR(x, 7) ^ ROTR(x, 18) ^ ((x) >> 3))
+#define sigma1(x) (ROTR(x, 17) ^ ROTR(x, 19) ^ ((x) >> 10))
+
+const uint32_t K[64] = {
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+    0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+    0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+    0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+    0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+    0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+    0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+};
+
+const uint32_t H[8] = {
+    0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a,
+    0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
+};
+
+void processBlock(const uint32_t *block, uint32_t *hash) {
+    uint32_t W[64];
+    uint32_t a, b, c, d, e, f, g, h, t1, t2;
+
+    memcpy(W, block, 64);
+
+    for (int t = 16; t < 64; t++) {
+        W[t] = sigma1(W[t - 2]) + W[t - 7] + sigma0(W[t - 15]) + W[t - 16];
+    }
+
+    a = hash[0];
+    b = hash[1];
+    c = hash[2];
+    d = hash[3];
+    e = hash[4];
+    f = hash[5];
+    g = hash[6];
+    h = hash[7];
+
+    for (int t = 0; t < 64; t++) {
+        t1 = h + Sigma1(e) + Ch(e, f, g) + K[t] + W[t];
+        t2 = Sigma0(a) + Maj(a, b, c);
+        h = g;
+        g = f;
+        f = e;
+        e = d + t1;
+        d = c;
+        c = b;
+        b = a;
+        a = t1 + t2;
+    }
+
+    hash[0] += a;
+    hash[1] += b;
+    hash[2] += c;
+    hash[3] += d;
+    hash[4] += e;
+    hash[5] += f;
+    hash[6] += g;
+    hash[7] += h;
+}
+
+void sha256(const uint8_t *message, size_t len, char *hash) {
+    size_t numBlocks = (len + 8) / 64 + 1;
+    uint32_t block[16];
+
+    uint32_t hashValues[8];
+    memcpy(hashValues, H, sizeof(H));
+
+    for (size_t i = 0; i < numBlocks; i++) {
+        size_t start = i * 64;
+        size_t end = start + 64;
+        if (end > len) {
+            end = len;
+        }
+
+        memset(block, 0, sizeof(block));
+        memcpy(block, message + start, end - start);
+
+        if (i == numBlocks - 1) {
+            block[(end - start) / 4] = len * 8;
+        }
+
+        processBlock(block, hashValues);
+    }
+
+    for (int i = 0; i < 8; i++) {
+        sprintf(hash + (i * 8), "%08x", hashValues[i]);
+    }
+}
+
+
